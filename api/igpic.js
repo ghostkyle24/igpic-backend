44367d3eb90defee32f70b8625955773845529b4
ghostkyle24 <painel@ftm.com>
2025-07-26 14:14:40 -0300

    alteração 2

--- api/igpic.js
+++ api/igpic.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite qualquer origem
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const { username } = req.query;
   if (!username) return res.status(400).json({ error: 'username required' });
 
